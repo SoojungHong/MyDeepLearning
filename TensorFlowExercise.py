e3bb11c2aa3d4507c77dd4f4c89ddc12d933f6e6
Soojung <hong.soojung@gmail.com>
2018-08-27 17:21:22 +0200

    manual gradient descent

--- TensorFlowExercise.py
+++ TensorFlowExercise.py
@@ -73,6 +73,7 @@ with tf.Session() as sess:
 
 
 # Linear Regression with TensorFlow 
+import tensorflow as tf    
 import numpy as np
 from sklearn.datasets import fetch_california_housing
 
@@ -85,7 +86,34 @@ housing_data_plus_bias = np.c_[np.ones((m,1)), housing.data] # np.c_ means : con
 X = tf.constant(housing_data_plus_bias, dtype=tf.float32, name="X")
 y = tf.constant(housing.target.reshape(-1,1), dtype=tf.float32, name="y")
 XT = tf.transpose(X)
-theta = tf.matmul(tf.matul(tf.matrix_inverse(tf.matmul(XT,X)), XT), y)
+theta = tf.matmul(tf.matmul(tf.matrix_inverse(tf.matmul(XT,X)), XT), y)
 
 with tf.Session() as sess:
     theta_value = theta.eval()
+    print(theta_value)
+    
+
+# Manually computing the gradients 
+n_epochs = 1000
+learning_rate = 0.01
+
+X = tf.constant(scaled_housing_data_plus_bias, dtype=tf.float32, name="X")
+y = tf.constant(housing.target.reshape(-1,1), dtype=tf.float32, name="y")
+theta = tf.Variable(tf.random_uniform([n+1,1], -1.0, 1.0), name="theta")
+y_pred = tf.matmul(X, theta, name="predictions")
+error = y_pred - y
+mse = tf.reduce_mean(tf.square(error), name="mse")
+gradients = 2/m * tf.matmul(tf.transpose(X), error)
+training_op = tf.assign(theta, theta - learning_rate * gradients)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    
+    for epoch in range(n_epochs):
+        if epoch % 100 == 0:
+            print("Epoch", epoch, "MSE=", mse.eval())
+        sess.run(training_op)
+    
+    best_theta = theta.eval()    
