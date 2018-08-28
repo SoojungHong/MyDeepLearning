1f5398960cc0ea688f30f805775922cd8c038599
Soojung <hong.soojung@gmail.com>
2018-08-28 18:02:15 +0200

    autodiff for gradient and using optimizer

--- TensorFlowExercise.py
+++ TensorFlowExercise.py
@@ -94,12 +94,23 @@ with tf.Session() as sess:
     
 
 # Manually computing the gradients 
+tf.reset_default_graph()    
 n_epochs = 1000
 learning_rate = 0.01
 
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaled_housing_data = scaler.fit_transform(housing.data)
+scaled_housing_data_plus_bias = np.c_[np.ones((m,1)), scaled_housing_data]
+
+print(scaled_housing_data_plus_bias.mean(axis=0)) # axis = 0 row 
+print(scaled_housing_data_plus_bias.mean(axis=1)) # axis = 1 column 
+print(scaled_housing_data_plus_bias.mean()) # axis = 1 column 
+print(scaled_housing_data_plus_bias.shape) # axis = 1 column 
+
 X = tf.constant(scaled_housing_data_plus_bias, dtype=tf.float32, name="X")
 y = tf.constant(housing.target.reshape(-1,1), dtype=tf.float32, name="y")
-theta = tf.Variable(tf.random_uniform([n+1,1], -1.0, 1.0), name="theta")
+theta = tf.Variable(tf.random_uniform([n+1,1], -1.0, 1.0, seed = 42), name="theta")
 y_pred = tf.matmul(X, theta, name="predictions")
 error = y_pred - y
 mse = tf.reduce_mean(tf.square(error), name="mse")
@@ -117,3 +128,65 @@ with tf.Session() as sess:
         sess.run(training_op)
     
     best_theta = theta.eval()    
+
+print("Best theta: ")
+print(best_theta) 
+
+# using autodiff 
+tf.reset_default_graph()
+
+n_epochs = 1000
+learning_rate = 0.01
+
+X = tf.constant(scaled_housing_data_plus_bias, dtype=tf.float32, name="X")
+y = tf.constant(housing.target.reshape(-1,1), dtype=tf.float32, name="y")
+theta = tf.Variable(tf.random_uniform([n+1, 1], -1.0, 1.0, seed=42), name="theta")
+y_pred = tf.matmul(X, theta, name="predictions")
+error = y_pred - y
+mse = tf.reduce_mean(tf.square(error), name="mse")
+gradients = tf.gradients(mse, [theta])[0]
+training_op = tf.assign(theta, theta - learning_rate * gradients)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    
+    for epoch in range(n_epochs):
+        if epoch % 100 == 0:
+            print("Epoch", epoch, "MSE =", mse.eval())
+        sess.run(training_op)
+    
+    best_theta = theta.eval()
+
+print("Best theta:")
+print(best_theta)    
+
+# using Gradient Descent Optimizer
+tf.reset_default_graph()
+n_epochs = 1000
+learning_rate = 0.01
+
+X = tf.constant(scaled_housing_data_plus_bias, dtype=tf.float32, name="X")
+y = tf.constant(housing.target.reshape(-1,1), dtype=tf.float32, name="y")
+theta = tf.Variable(tf.random_uniform([n+1,1], -1.0, 1.0, seed=42), name="theta")
+y_pred = tf.matmul(X, theta, name="predictions")
+error = y_pred - y
+mse = tf.reduce_mean(tf.square(error), name="mse")
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+training_op = optimizer.minimize(mse)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    
+    for epoch in range(n_epochs):
+        if epoch % 100 == 0:
+            print("Epoch", epoch, "MSE =", mse.eval())
+        sess.run(training_op)
+    
+    best_theta = theta.eval()
+
+print("Best theta:")
+print(best_theta)    
