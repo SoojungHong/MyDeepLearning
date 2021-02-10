05e900bcb816e53aa0ccb3705ad75a10dd2de61d
Soojung Hong <hong.soojung@gmail.com>
2021-02-10 21:08:48 +0100

    Create Networks.py

---
+++ Models/Networks.py
@@ -0,0 +1,10 @@
+"""
+Define each network in this file and let the network is called by get_...() 
+"""
+
+def get_generator():
+    print("get generator is called.....")
+
+
+def get_discriminator():
+    print("get discriminator is called")
