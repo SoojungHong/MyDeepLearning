26086c02ed0f36e0844197442e56681c6d1f8904
Soojung Hong <hong.soojung@gmail.com>
2021-04-15 20:48:04 +0200

    Update Tensor_to_ndarray_and_image.py

--- Tensor_to_ndarray_and_image.py
+++ Tensor_to_ndarray_and_image.py
@@ -1,19 +1,28 @@
 # 'batch_pos' is <class 'tensorflow.python.framework.ops.Tensor'> type 
-init_op = tf.global_variables_initializer()
-with tf.compat.v1.Session().as_default() as sess:
-  sess.run(init_op)
-  coord = tf.train.Coordinator()
-  threads = tf.train.start_queue_runners(coord=coord)
-  try:
-    while not coord.should_stop():
-      array = sess.run(batch_pos) # Tensor to ndarray 
-      print("array : ", array)
-      print("type(array) : ", type(array))
+ def tensor_to_image(self, input_tensor, img_name):
+      print("debug 1 : type(input_tensor) is ", type(input_tensor)) 
+      
+      init_op = tf.global_variables_initializer()
+      with tf.compat.v1.Session().as_default() as sess:
+        sess.run(init_op)
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(coord=coord)
+        try:
+          #while not coord.should_stop():
+            array = sess.run(input_tensor)
+            print("debug 2 : ", array)
+            print("debug 3 : ", type(array))
+            
+            arr_ = np.squeeze(array) # you can give axis attribute if you wanna squeeze in specific dimension
+            plt.imshow(arr_)
+            #plt.show()
+            plt.savefig(img_name+'.png')
               
-      img = Image.fromarray(array[0], "RGB") # ndarray to image
-      img.save("sj_test.png")
-  except Exception as e:
-    coord.request_stop(e)
-  finally:
-    coord.request_stop()
-    coord.join(threads)
+            #img = Image.fromarray(array[0], "RGB") # This saved image looks wired
+            #img.save(img_name+".png")
+        except Exception as e:
+          coord.request_stop(e)
+        finally:
+          coord.request_stop()
+          coord.join(threads) 
+    
